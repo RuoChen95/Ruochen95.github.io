b64fe909471652ef8ec57473edd4009d2397171a
Xie Ruochen <xrc_1995@163.com>
2017-04-04 17:27:16 +0800

    blog page: rewrite the article using js and dom; fix some bug

---
+++ html/blog/js/helper.js
@@ -0,0 +1,9 @@
+var HTMLBlogHead = '<header class = "blogHead"></header>';
+var HTMLBlogSenconTitle = '<div class="byline secondaryText"></div>';
+
+
+var HTMLBlogTitle = '<h1 class = "title">%data%</h1>';
+var HTMLBlogOrder = '<p>#%data%</p>'
+var HTMLBlogTime = '<p>%data%</p>'
+var HTMLBlogContent = '<div class = "content">%data%</div>'
+
