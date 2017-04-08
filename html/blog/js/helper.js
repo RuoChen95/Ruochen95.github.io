661e354ce2ea77e8c5ff6a8c4ef13253b0248aa5
Xie Ruochen <xrc_1995@163.com>
2017-04-08 10:32:14 +0800

    jekyll-now trying

--- html/blog/js/helper.js
+++
@@ -1,9 +0,0 @@
-var HTMLBlogHead = '<header class = "blogHead"></header>';
-var HTMLBlogSenconTitle = '<div class="byline secondaryText"></div>';
-
-
-var HTMLBlogTitle = '<h1 class = "title">%data%</h1>';
-var HTMLBlogOrder = '<p>#%data%</p>'
-var HTMLBlogTime = '<p>%data%</p>'
-var HTMLBlogContent = '<div class = "content">%data%</div>'
-
